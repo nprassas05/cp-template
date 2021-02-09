96ffd8b72d4936a57ab8f677379d2f7b5ce4fecd
Niko Prassas <prassasniko@gmail.com>
2021-02-09 14:32:07 -0500

    add another way to do sieve

--- sieve.cpp
+++ sieve.cpp
@@ -1,8 +1,11 @@
-set<int> primes;
+#include <bits/stdc++.h>
+using namespace std;
 
-void sieve(int n) 
+void sieve_old(const int n) 
 {
-    bool prime[n+1]; 
+    set<int> primes;
+    const int max_n = 30303;
+    bool prime[max_n]; 
     memset(prime, true, sizeof(prime)); 
   
     for (int p=2; p*p<=n; p++) 
@@ -19,4 +22,23 @@ void sieve(int n)
     //~ for (int p=2; p<=n; p++) 
        //~ if (prime[p]) 
           //~ cout << p << endl;
+}
+
+const int N = 30303;
+void sieve() {
+    vector<int> primes;
+    primes.reserve(N);
+    vector<bool> isPrime(N);
+    for (int i = 2; i < N; i++)
+            isPrime[i] = 1;
+    for (int x = 2; x < N; x++) {
+        if (!isPrime[x]) continue;
+        primes.push_back(x);
+        for (int y = 2 * x; y < N; y += x)
+            isPrime[y] = 0;
+    }
+}
+
+int main() {
+    return 0;
 }
