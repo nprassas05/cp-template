fbb0c62f954d50f0cd11d088606be801cdfeb627
Niko Prassas <prassasniko@gmail.com>
2021-07-12 21:03:17 -0400

    new segtree implementation

--- segtree.cpp
+++ segtree.cpp
@@ -1,104 +1,59 @@
-class rmq_segtree {
-private:
-	size_t tree_size;
-	size_t elems_size;
-	
-	vector<int> elems;
-	vector<int> tree;
-	
-	void build_tree(int low, int high, int idx) {
-		assert(low <= high);
-		assert(0 <= idx && idx < tree.size());
-		
-		if (low == high) {
-			tree[idx] = low;
-			return;
-		}
-		
-		int mid = low + (high - low) / 2;
-		int left_child = idx * 2 + 1;
-		int right_child = idx * 2 + 2;
-		
-		build_tree(low, mid, left_child);
-		build_tree(mid + 1, high, right_child);
-		
-		if (elems[tree[left_child]] > elems[tree[right_child]]) {
-			tree[idx] = tree[left_child];
-		} else {
-			tree[idx] = tree[right_child];
-		}
+template<typename T>
+struct segtree {
+	int n;
+	vector<T> tree;
+
+    const T dummy_val = 0;
+
+    T unite(T left_val, T right_val) {
+        return std::gcd(left_val, right_val);
+    }
+	void recalc(int p) {
+		tree[p] = unite(tree[p * 2], tree[p * 2 + 1]);
 	}
-	
-	/*
-	 * Answer the range maximum query in the inclusive range [i,  j]
-	 * [i, j] the range we are querying
-	 * [low, high] the current index range we are in
-	 * idx the index that corresponds to the range we are currently in
-	 */
-	int rmq(int i, int j, int low, int high, int idx) {
-		assert(i <= j);
-		assert(low <= high);
-		assert(0 <= idx && idx <= tree.size());
-		
-		if (i == j) {
-			return i;
+
+	void build(const vector<T> & elems) {
+		this->n = 1;
+
+		while (n < (int) elems.size()) {
+			n *= 2;
 		}
-		
-		// current range is completely outside the query range
-		// return invalid result
-		if (high < i || low > j) {
-			return -1;
-		}
-		
-		
-		// current range is completely inside the query range
-		// this seems to cover the case of when low == high
-		if (i <= low && high <= j) {
-			return tree[idx];
-		}
-		
-		int mid = low + (high - low) / 2;
-		int left_child = idx * 2 + 1;
-		int right_child = idx * 2 + 2;
-		
-		// if we get here,  it means that part of our current range
-		// contains part of the range of the query, but some of
-		// our range is outside the query, so we need to further trim
-		// it down.
-		int left_result = rmq(i, j, low, mid, left_child);
-		int right_result = rmq(i, j, mid + 1, high, right_child);
-		
-		assert((0 <= left_result && left_result < elems_size) || left_result ==  -1);
-		assert((0 <= right_result && right_result < elems_size) || right_result == -1);
-		
-		if (left_result == -1) {
-			return right_result;
-		}
-		if (right_result == -1) {
-			return left_result;
-		}
-		if (elems[left_result] > elems[right_result]) {
-			return left_result;
-		} else {
-			return right_result;
-		}
+
+        tree.assign(2 * n, dummy_val);
+        for (int i = 0; i < (int) elems.size(); ++i) {
+            tree[n + i] = elems[i];
+        }
+        for (int i = n - 1; i >= 1; --i) {
+            recalc(i);
+        }
 	}
-public:
-	rmq_segtree(const vector<int> &vec) {
-		elems = vec;
-		elems_size = elems.size();
-		tree.resize(4 * elems_size);
-		tree_size = 4 * elems_size;
-		build_tree(0, elems_size - 1, 0);
+
+	T query(int qlow, int qhigh, int rlow, int rhigh, int tree_idx) {
+         // inside
+        if (qlow <= rlow && rhigh <= qhigh) {
+            return tree[tree_idx];
+        }
+        // disjoint
+        if (rlow > qhigh || rhigh < qlow) {
+            return dummy_val;
+        }
+
+        int left_child = tree_idx * 2;
+        int right_child = tree_idx * 2 + 1;
+        int mid = (rlow + rhigh) / 2;
+ 
+        return unite(query(qlow, qhigh, rlow, mid, left_child),
+                     query(qlow, qhigh, mid + 1, rhigh, right_child));
 	}
-	
-	int max_in_range(int i, int j) {
-		return elems[rmq(i, j)];
-	}
-	
-	int rmq(int i, int j) {
-		assert(i <= j);
-		
-		return rmq(i, j, 0, elems_size - 1, 0);
+
+	T query(const int low, const int high) {
+        return query(low, high, 0, this->n - 1, 1);
+    }
+
+	void update(int idx, int val) {
+        tree[n + idx] = val;
+        for (int p = (n + idx) / 2; p > 0; p /= 2) {
+            recalc(p);
+        }
 	}
 };
